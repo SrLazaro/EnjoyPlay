145160d503b78986e8439b548509892019056a3a
SrLazaro <lazarosobreira@gmail.com>
2022-08-21 14:55:47 -0300

    first commit

---
+++ js/Init.js
@@ -0,0 +1,71 @@
+class Init extends Phaser.Scene{
+    constructor() {
+        super('Init')
+    }
+
+    preload(){
+       
+        this.load.image('initial_screen', '/img/initial_screen.png')
+
+        this.load.image('play_button', '/img/play_button.png')
+        //this.load.spritesheet('play_button', '/img/play_button.png', 193, 71);
+
+        this.load.image('options_button', '/img/options_button.png')
+
+        this.load.audio('title_music', './music/title.mp3')
+        
+    }
+
+    create(){
+
+        this.initial_screen = this.add.image(0,0,'initial_screen').setOrigin(0,0)
+
+        this.play_button = this.add.image(400,440, 'play_button')
+        this.play_button.setInteractive()
+        this.play_button.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+            console.log('pressioned')
+        })
+
+        this.options_button = this.add.image(400,480,'options_button')
+
+        this.play_song('title_music')
+        
+    }
+
+    update(){
+
+        
+
+    }
+
+    play_song(name_music){
+
+        this.sound.pauseOnBlur = false
+
+        this.music =  this.sound.add(name_music, {
+            volume: 0.2,
+            loop: true
+        })
+
+        this.music.play()
+    }
+
+}
+
+function up() {
+    console.log('button up', arguments);
+}
+
+function over() {
+    console.log('button over');
+}
+
+function out() {
+    console.log('button out');
+}
+
+function actionOnClick () {
+
+    background.visible =! background.visible;
+
+}
---
+++ js/main.js
@@ -0,0 +1,14 @@
+let game
+
+window.onload = function(){
+
+    const config = {
+        type: Phaser.Canvas,
+        width: 800,
+        height: 600,
+        scene: [Init]
+    }
+
+    game = new Phaser.Game(config)
+
+}
