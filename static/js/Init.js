c38b338e4f8096765d8f2751f4de3651ee19f952
SrLazaro <lazarosobreira@gmail.com>
2022-09-04 14:30:49 -0300

    modificação só o ouro

--- static/js/Init.js
+++ static/js/Init.js
@@ -26,9 +26,11 @@ class Init extends Phaser.Scene{
 
         this.play_button = this.add.image(400,440, 'play_button')
         this.play_button.setInteractive()
+
         this.play_button.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
             this.play_song('click_song', false)
-            console.log('pressioned')
+            this.music.pause()
+            this.scene.start('Scene01')
             
         })
 
@@ -55,23 +57,5 @@ class Init extends Phaser.Scene{
 
         this.music.play()
     }
-
-}
-
-function up() {
-    console.log('button up', arguments);
-}
-
-function over() {
-    console.log('button over');
-}
-
-function out() {
-    console.log('button out');
-}
-
-function actionOnClick () {
-
-    background.visible =! background.visible;
 
 }
