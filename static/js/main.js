b050d2bbfae820aa9c795b20689379dcc4e112f3
SrLazaro <lazarosobreira@gmail.com>
2022-09-18 19:14:07 -0300

    testes

--- static/js/main.js
+++ static/js/main.js
@@ -4,8 +4,8 @@ window.onload = function(){
 
     const config = {
         type: Phaser.Canvas,
-        width: 800,
-        height: 600,
+        width: 921,
+        height: 870,
         scene: [Init,
                 Scene01]
     }
